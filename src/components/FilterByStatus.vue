228c203295767840fab5238213e9f8a6cd969267
maxi <maxidy71@hotmail.com>
2024-08-03 20:30:11 -0300

    primer

---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="filter">
+        <div class="item" @click="$event => filter('')">
+            All
+        </div>
+        <div class="item" @click="$event => filter('Alive')">
+            Alive
+        </div>
+        <div class="item" @click="$event => filter('Dead')">
+            Died
+        </div>
+        <div class="item" @click="$event => filter('Unknow')">
+            Unknow
+        </div>
+    </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+    setup() {
+        const store = useStore()
+
+        const filter = ((status) => {
+            store.dispatch('filterByStatus', status)
+        })
+    }
+}
+</script>
+
+<style lang="scss">
+.filter {
+  width: 400px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  border-radius: 10px;
+  overflow: hidden;
+  .item {
+    padding: 1rem 0.5rem;
+    background-color: var(--background-card);
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      color: var(--text-orange);
+    }
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,46 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters:[],
+    charactersFilter:[],
   },
-  getters: {
-  },
-  mutations: {
+    mutations: {
+      setCharacters(state, payload){
+        state.characters = payload
+      },
+      setCharactersFilter(state, payload){
+          state.charactersFilter = payload
+      }
   },
   actions: {
+    async getCharacters({commit}) {
+      try{
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+          const data = await response.json()
+          commit('setCharacters', data.results)
+          commit('setCharactersFilter', data.results)
+      }catch(error){
+        console.error(error)
+      }
+    },
+    filterByStatus({commit, state}, status){
+        const filter = state.charactersfilter((character) => {
+              return character.status.includes(status,)
+        })
+        commit('setCharactersFilter', filter)
+    },
+
+    filterByName({commit, state}, name){
+        const formatName = name.tolowercase()
+        const filter = state.characters.filter((character) => {
+            const characterName = character.name.tolowercase()
+
+            if(characterName.includes(formatName)){
+              return character
+            }
+        })
+        commit('setCharactersFilter', filter)
+    }
   },
   modules: {
   }
